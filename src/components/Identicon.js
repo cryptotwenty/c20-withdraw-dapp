243510d58f0438375b7c7b383817408f995e37d7
Jason Smythe <jason@smythemail.za.net>
2018-01-11 00:30:56 +0200

    Fix: Identicon loads correctly

--- src/components/Identicon.js
+++ src/components/Identicon.js
@@ -2,20 +2,31 @@ import React, { Component } from 'react'
 import jazzicon from 'jazzicon'
 
 export default class Identicon extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {hasLoaded: false};
+  }
+
+  loadIdenticon() {
+    const {
+      address,
+      diameter
+    } = this.props
+    if (address !== '0x0000000000000000000000000000000000000000' && !this.state.hasLoaded) {
+      const addresToInt = parseInt(
+        address.slice(2, 10),
+        16
+      )
+      this.refs['identicon-' + address].append(jazzicon(diameter, addresToInt))
+      this.setState({hasLoaded: true})
+    }
+  }
   componentDidMount() {
-    const addresToInt = parseInt(
-      this.props.address.slice(2, 10),
-      16
-    )
-    this.refs['identicon-'+this.props.address].append(jazzicon(this.props.diameter, addresToInt))
+    this.loadIdenticon()
   }
 
   componentDidUpdate() {
-    const addresToInt = parseInt(
-      this.props.address.slice(2, 10),
-      16
-    )
-    this.refs['identicon-'+this.props.address].append(jazzicon(this.props.diameter, addresToInt))
+    this.loadIdenticon()
   }
   render() {
     return(
