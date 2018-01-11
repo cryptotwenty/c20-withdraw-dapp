c7459574055f66a8805b8ecb1f18e8acfcd159a5
Jason Smythe <jason@smythemail.za.net>
2018-01-11 22:46:09 +0200

    Remove styling for deploy

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 import {Provider} from "react-redux"
 
-import "./index.css"
+// import "./index.css" // remove this for live deploy
 import App from "./App"
 import InstanceWrapper from './InstanceWrapper'
 import configureStore from "./configureStore"
